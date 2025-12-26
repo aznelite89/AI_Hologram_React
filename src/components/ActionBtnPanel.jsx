c6455ab17e46834c9f90206a4817ede30deef8de
Chan Thai Thong <thongz0819@live.com>
2025-12-26 08:48:46 +0800

    - larger buttons, invisible padding, kiosk click enhancements

--- src/components/ActionBtnPanel.jsx
+++ src/components/ActionBtnPanel.jsx
@@ -66,7 +66,7 @@ const ActionBtnPanel = () => {
     <>
       <div id="action-buttons-container">
         <div className="button-instruction">Tap to Reset Conversation</div>
-        <button id="btn-refresh-conversation" onClick={onReset}>
+        <button id="btn-refresh-conversation" onPointerDown={onReset}>
           <svg width="60" height="60" viewBox="0 0 24 24" fill="none">
             <path
               d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-0.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14 0.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
@@ -78,7 +78,7 @@ const ActionBtnPanel = () => {
         <button
           id="btn-main-microphone"
           className={micClassName}
-          onClick={onMic}
+          onPointerDown={onMic}
         >
           {isProcessing ? (
             <i className="fas fa-spinner fa-spin"></i>
