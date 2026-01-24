940b2fae18cf69347a5f392b433ca2a8b8334274
Chan Thai Thong <thongz0819@live.com>
2026-01-24 14:15:40 +0800

    - remove floor switcher

--- src/components/MappedinMap.jsx
+++ src/components/MappedinMap.jsx
@@ -8,7 +8,7 @@ const MappedinMap = () => {
   const { isLoading, error, mapData } = useMapData({
     key: "mik_DUwlsWsBypbdww8je5ad50840",
     secret: "mis_dylRwkoXQb3ocvaZURE20d0wQLJ6BgEINpYw9t9EQNy9a0b1054",
-    mapId: "68edec68d24915000bbf8757",
+    mapId: "68edec68d24915000bbf8757"
   })
 
   if (isLoading) return <div>Loading indoor map…</div>
@@ -18,7 +18,7 @@ const MappedinMap = () => {
     <div id="map">
       <MapView mapData={mapData} style={{ height: "100%", width: "100%" }}>
         <div className="map-ui-layer">
-          <FloorSwitcher />
+          {/* <FloorSwitcher /> */}
           <MapOverlay />
         </div>
       </MapView>
